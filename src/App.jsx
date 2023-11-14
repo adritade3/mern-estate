f0028174caed99428e4a7f82124cd559b7dcba36
adritade3 <de.adrita3@gmail.com>
2023-11-14 20:48:50 +0530

    crete pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
-import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignOut from "./pages/SignUpjsx";
+import Signin from "./pages/Signin";
+import About from "./pages/About";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/sign-out" element={<SignOut />}></Route>
+        <Route path="/sign-in" element={<Signin />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
