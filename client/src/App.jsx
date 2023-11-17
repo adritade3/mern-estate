2d344651a936d887b9e19f47f12712c4aef37576
adritade3 <de.adrita3@gmail.com>
2023-11-17 21:45:22 +0530

    complete signup page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
-import SignOut from "./pages/SignUp.jsx";
 import Signin from "./pages/Signin";
 import About from "./pages/About";
 import Header from "./components/header";
+import SignUp from "./pages/SignUp.jsx";
 
 export default function App() {
   return (
@@ -13,7 +13,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/profile" element={<Profile />}></Route>
-        <Route path="/sign-out" element={<SignOut />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
         <Route path="/sign-in" element={<Signin />}></Route>
         <Route path="/about" element={<About />}></Route>
       </Routes>
