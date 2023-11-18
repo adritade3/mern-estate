21c506128842034b03c0a9207a8d51116c35f98e
adritade3 <de.adrita3@gmail.com>
2023-11-18 10:42:49 +0530

    create signin api route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, password, email } = req.body;
@@ -11,6 +12,28 @@ export const signup = async (req, res, next) => {
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
     //next(errorHandler(550, "error from the function"));
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword)
+      return next(errorHandler(404, "Invalid username and password"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      })
+      .status(200)
+      .json(rest);
+  } catch (error) {
     next(error);
   }
 };
