66f3502a9ee7e49066e3b8b149799928c413ba5e
adritade3 <de.adrita3@gmail.com>
2023-11-20 13:12:48 +0530

    complete listing ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import About from "./pages/About";
 import Header from "./components/header";
 import SignUp from "./pages/SignUp.jsx";
 import PrivateRoute from "./components/PrivateRoute.jsx";
+import CreateListing from "./pages/CreateListing.jsx";
 
 export default function App() {
   return (
@@ -15,7 +16,9 @@ export default function App() {
         <Route path="/" element={<Home />}></Route>
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/create-listing" element={<CreateListing />}></Route>
         </Route>
+
         <Route path="/sign-up" element={<SignUp />}></Route>
         <Route path="/sign-in" element={<Signin />}></Route>
         <Route path="/about" element={<About />}></Route>
